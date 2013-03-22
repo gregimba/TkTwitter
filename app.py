f42d083669ce9ddcb4418870f75e3f98e0f47988
Grant Regimbal <regimbal.grant@gmail.com>
2013-03-21 17:00:42 -0700

    working now

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
+import toml
 from twitter import *
 from Tkinter import *
 
+config = open("config.toml", "r")
+config = toml.loads(config.read())
 
- 
+OAUTH_TOKEN = config["OAUTH_TOKEN"]
+OAUTH_SECRET = config["OAUTH_SECRET"]
+CONSUMER_KEY = config["CONSUMER_KEY"]
+CONSUMER_SECRET = config["CONSUMER_SECRET"]
+
 twitter = Twitter(
               auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET,
 	                       CONSUMER_KEY, CONSUMER_SECRET)
 	           )
- 
 
 class App:
  
     def __init__(self, master):
-        global textStatus
+        global tweet
         frame = Frame(master)
         frame.pack()
 
-        textStatus = StringVar()
-        self.statusEntry = Entry(frame, textvariable=textStatus)
+        tweet = StringVar()
+        self.statusEntry = Entry(frame, textvariable=tweet)
         self.statusEntry.pack()
  
         self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
@@ -27,9 +33,8 @@ class App:
         self.Tweet.pack(side=LEFT)
  
     def tweet(self):
-        twitter.statuses.update(status=textStatus)
- 
+        twitter.statuses.update(status=tweet.get())
+
 root = Tk()
 app = App(root)
- 
 root.mainloop()
